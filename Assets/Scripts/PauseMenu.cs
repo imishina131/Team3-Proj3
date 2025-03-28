e80a9005b365b288daf9fcd5a854ecd2fcd2999c
Irina Mishina <irinamishina131@gmail.com>
2025-03-28 15:34:52 -0400

    pause menu added in all levels as well as tutorial

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isPaused;
+    
+    public GameObject pauseMenuUI;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+}
