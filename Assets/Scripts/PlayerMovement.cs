3e486a54e5c22a062873467c77103bb623033b10
Irina Mishina <irinamishina131@gmail.com>
2025-03-31 23:04:12 -0400

    pathfinder mechanic coded in tutorial

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,111 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float playerSpeed = 2.0f;
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool movingUp;
+    private bool movingDown;
+    private bool movingRight;
+    private bool movingLeft;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        controller = gameObject.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChoosingDirection();
+
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "Wall")
+        {
+            Debug.Log("WALL");
+            if(movingUp)
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + 0.4f);
+            }
+            else if(movingDown)
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - 0.4f);
+            }
+            else if(movingLeft)
+            {
+                transform.position = new Vector3(transform.position.x - 0.4f, transform.position.y, transform.position.z);
+            }
+            else if(movingRight)
+            {
+                transform.position = new Vector3(transform.position.x + 0.4f, transform.position.y, transform.position.z);
+            }
+            movingUp = false;
+            movingDown = false;
+            movingLeft = false;
+            movingRight = false;
+        }
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Finish")
+        {
+            if(SceneManager.GetActiveScene().name == "Tutorial")
+            {
+                SceneManager.LoadScene("Level01");
+            }     
+            else if(SceneManager.GetActiveScene().name == "Level01")
+            {
+                SceneManager.LoadScene("Level02");
+            }
+        }
+    }
+
+    void ChoosingDirection()
+    {
+        if(Input.GetKeyDown(KeyCode.W) && !movingUp && !movingDown && !movingLeft && !movingRight)
+        {
+            movingUp = true;
+        }
+        else if(Input.GetKeyDown(KeyCode.A) && !movingUp && !movingDown && !movingLeft && !movingRight)
+        {
+            movingLeft = true;
+        }
+        else if(Input.GetKeyDown(KeyCode.D) && !movingUp && !movingDown && !movingLeft && !movingRight)
+        {
+            movingRight = true;
+        }
+        else if(Input.GetKeyDown(KeyCode.S) && !movingUp && !movingDown && !movingLeft && !movingRight)
+        {
+            movingDown = true;
+        }
+
+        Moving();
+    }
+
+    void Moving()
+    {
+        if(movingUp)
+        {
+            transform.Translate(Vector3.back * playerSpeed * Time.deltaTime);
+        }
+        else if(movingDown)
+        {
+            transform.Translate(Vector3.forward * playerSpeed * Time.deltaTime);
+        }
+        else if(movingLeft)
+        {
+            transform.Translate(Vector3.right * playerSpeed * Time.deltaTime);
+        }
+        else if(movingRight)
+        {
+            transform.Translate(Vector3.left * playerSpeed * Time.deltaTime);
+        }
+    }
+}
