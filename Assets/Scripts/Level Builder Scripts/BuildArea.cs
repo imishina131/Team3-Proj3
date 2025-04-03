8be5c3550325ad96929017388fc8ef0adc8d7565
LilyRead <lgread@my.waketech.edu>
2025-04-03 14:58:13 -0400

    level editor update

--- Assets/Scripts/Level Builder Scripts/BuildArea.cs
+++ Assets/Scripts/Level Builder Scripts/BuildArea.cs
@@ -4,7 +4,7 @@ using UnityEditor;
 public class BuildArea : MonoBehaviour
 {
     [HideInInspector] public float tileSize;
-    [HideInInspector] public Vector2 size;
+    [HideInInspector] public Vector3 size;
 
     public bool showBuildAreaGizmo = true;
     public bool showTileTopGizmo = true;
@@ -14,18 +14,21 @@ public class BuildArea : MonoBehaviour
     {
         for (int x = 0; x < size.x; x++)
         {
-            for (int z = 0; z < size.y; z++)
+            for (int z = 0; z < size.z; z++)
             {
-                Vector3 pos = new Vector3(transform.position.x + (x * tileSize), transform.position.y, transform.position.z + (z * tileSize));
-                GameObject node = new GameObject();
-                node.transform.parent = transform;
-                node.transform.position = pos;
-                node.name = $"Build Node (X: {x}, Z: {z})";
-                Node nodescript = node.AddComponent<Node>();
-                nodescript.buildArea = this;
-                #if UNITY_EDITOR
+                for (int y = 0; y < size.y; y++)
+                {
+                    Vector3 pos = new Vector3(transform.position.x + (x * tileSize), transform.position.y + (y * tileSize), transform.position.z + (z * tileSize));
+                    GameObject node = new GameObject();
+                    node.transform.parent = transform;
+                    node.transform.position = pos;
+                    node.name = $"Build Node (X: {x}, Z: {z}), Y: {y}";
+                    Node nodescript = node.AddComponent<Node>();
+                    nodescript.buildArea = this;
+#if UNITY_EDITOR
                     UnityEditor.EditorUtility.SetDirty(node);
-                #endif
+#endif
+                }
             }
         }
     }
@@ -37,8 +40,11 @@ public class BuildArea : MonoBehaviour
         {
             for (int z = 0;z < size.y; z++)
             {
-                Vector3 pos = new Vector3(transform.position.x + (x * tileSize), transform.position.y, transform.position.z + (z * tileSize));
-                Gizmos.DrawWireCube(pos, new Vector3(tileSize, 0, tileSize));
+                for (int y = 0; y < size.y; y++)
+                {
+                    Vector3 pos = new Vector3(transform.position.x + (x * tileSize), transform.position.y, transform.position.z + (z * tileSize));
+                    Gizmos.DrawWireCube(pos, new Vector3(tileSize, 0, tileSize));
+                }
             }
         }
     }
