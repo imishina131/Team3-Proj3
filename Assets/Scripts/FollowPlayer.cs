d5fbb1ff44c1bacafeaa6b35137b26a2105b286b
Irina Mishina <irinamishina131@gmail.com>
2025-04-30 16:52:38 -0400

    message follows player and stays straight

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 1.5f, player.transform.position.z - 1.0f);
+    }
+}
